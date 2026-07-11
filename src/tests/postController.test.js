0057821de100ddcb63a68814db4156cdc9a47704
Nat250 <johannesmk@uol.com.br>
2026-07-11 00:14:49 -0300

    ID25 - Testes para criação de posts

---
+++ src/tests/postController.test.js
@@ -0,0 +1,41 @@
+//Testa a criacao, edicao e exclusai de posts
+//Atividades ID 25, 26 e 27
+
+//Atividade ID 25
+describe("Tests post creation", () => {
+    var req, res, title, content, author;
+    it("should verify the post with a lack of title, content, or author", async () => {
+        req = {title, content};
+        res = {
+        status: function(responseStatus) {
+            expect(responseStatus).toBe(400);
+            return this; 
+        },
+        json: function(responseString){
+            expect(responseString).toStrictEqual({ error: 'Campos obrigatórios ausentes: title, content e author.' })
+        }};
+    });
+
+    it("should create an error for a post that has something other than the intented variables in the request", async () => {
+        req = 'test string';
+        res = {
+        status: function(responseStatus) {
+            expect(responseStatus).toBe(500);
+            return this; 
+        },
+        json: function(responseString){
+            expect(responseString).toStrictEqual({ error: 'Erro ao criar post.' })
+        }};
+    });
+
+    it("should create a new post if the title, content, and author veriables are all present", async () => {
+        req = {title: "Paragraphs",
+            content: "A book about words.",
+            author: "John Doe"};
+        res = {
+        status: function(responseStatus) {
+            expect(responseStatus).toBe(201);
+            return this; 
+        }};
+    })
+});
